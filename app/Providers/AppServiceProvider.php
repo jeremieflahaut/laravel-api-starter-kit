1905ec4632efb500f2c29fb09c8bc314213d86f2
jflahaut <30655859+jeremieflahaut@users.noreply.github.com>
2023-09-13 15:07:31 +0200

    tests workflow (#1)
    * tests
    
    * fix test example
    
    * Readme
    
    * fix typo
    
    * badges
    
    * badges
    
    * typo

--- app/Providers/AppServiceProvider.php
+++ app/Providers/AppServiceProvider.php
@@ -4,6 +4,8 @@ declare(strict_types=1);
 
 namespace App\Providers;
 
+use Illuminate\Database\Eloquent\Model;
+use Illuminate\Support\Facades\URL;
 use Illuminate\Support\ServiceProvider;
 
 class AppServiceProvider extends ServiceProvider
@@ -21,6 +23,10 @@ class AppServiceProvider extends ServiceProvider
      */
     public function boot(): void
     {
-        //
+        Model::preventLazyLoading(! $this->app->isProduction());
+
+        if ($this->app->isProduction()) {
+            URL::forceScheme('https');
+        }
     }
 }
